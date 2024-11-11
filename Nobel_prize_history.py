be903dff1a46c3ef9795cf9a4ee5bb53ce5fb5f6
Obsone <jsdcone49@gmail.com>
2024-11-12 02:36:08 +0800

    jupyter practice

--- Nobel_prize_history.py
+++ Nobel_prize_history.py
@@ -1,1 +1,10 @@
-# Intial commit test
+# Loading in required libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+nobel = pd.read_csv('nobel.csv')
+
+# What is the most commonly awarded gender and birth country?
+top_gender = nobel['sex'].value_counts().index[0]
