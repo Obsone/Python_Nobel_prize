0f4951f7565cba44fd0c4736f6c632c460ab9244
Obsone <jsdcone49@gmail.com>
2024-11-13 02:24:25 +0800

    docx us_winners

--- Nobel_prize_history.py
+++ Nobel_prize_history.py
@@ -8,3 +8,15 @@ nobel = pd.read_csv('nobel.csv')
 
 # What is the most commonly awarded gender and birth country?
 top_gender = nobel['sex'].value_counts().index[0]
+top_country = nobel['birth_country'].value_counts().index[0]
+
+# Which decade and Nobel Prize category combination had the highest proportion of female laureates?
+nobel["us_winners"] = nobel["birth_country"] == "United States of America"
+nobel["decade"] = (np.floor(nobel['year']/10) * 10).astype(int)
+prop_us_winners = nobel.groupby("decade", as_index=False)["us_winners"].mean()
+
+# setting as_index=False, you make sure the result is saved as a DataFrame rather than a series
+print(prop_us_winners)
+
+max_decade_usa = prop_us_winners[prop_us_winners["us_winners"] == prop_us_winners["us_winners"].max()]['decade'].values[0]
+print(max_decade_usa)
